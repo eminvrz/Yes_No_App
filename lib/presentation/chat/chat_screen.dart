c28cc0e816f2d0d0c7a616d3a6810500b9ee60a6
Emiliano <emiliano.nvrz@gmail.com>
2023-09-08 12:01:12 -0700

    ListView - area de mensajes

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -15,6 +15,30 @@ class ChatScreen extends StatelessWidget {
         ),
         title: const Text('Mi amor <3'),
       ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric( horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text('Indice: $index');
+              },
+            )),
+            const  Text('Mundo')
+          ],
+        ),
+      ),
     );
   }
 }

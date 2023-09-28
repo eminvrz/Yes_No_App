67968898907bf5082a1f6da976e846776d69bd4b
Emiliano <emiliano.nvrz@gmail.com>
2023-09-28 08:37:21 -0700

    Mostrar mensajes del ChatProvider

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
@@ -27,6 +30,9 @@ class _ChatView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Que instancia provider quiero usar?? ChatProvider
+    final chatProvider = context.watch<ChatProvider>();
+
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric( 
@@ -34,13 +40,16 @@ class _ChatView extends StatelessWidget {
         child: Column(
           children: [
             Expanded(child: ListView.builder(
-              itemCount: 100,
+              itemCount: chatProvider.messageList.length, // Aqui vere la cantidad de mensajes que hay
               itemBuilder: (context, index) {
-                return (index % 2 == 0)
-                  ? const HerMessageBubble()
-                  : const MyMessageBubble();
-              },
-            )),
+                final message = chatProvider.messageList[index];
+                return ( message.fromWho == FromWho.hers) // si es mensaje de ella mandare el HerMessageBubble
+                    ? const HerMessageBubble()
+                    : const MyMessageBubble(); // si no, mandare mi MessageBubble
+               // return (index % 2 == 0)
+               //   ? const HerMessageBubble()
+               //   : const MyMessageBubble();
+              })),
             // Caja de texto
             const MessageFieldBox(),
           ],

18a0371bf00c2ac02e7cdd84f7bcf55ae04c2489
Emiliano <emiliano.nvrz@gmail.com>
2023-09-28 19:21:56 -0700

    Respuesta de YesNo por Dio

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
 
+  // Instancias
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
   final ScrollController chatScrollController = ScrollController();
 
   List<Message> messageList = [
      Message(text: 'Hola amor!', fromWho: FromWho.me),
      Message(text: 'Ya regresastes del trabajo?', fromWho: FromWho.me),
   ];
+
+  // Metodos
 
   Future<void> sendMessage(String text) async{
 
@@ -17,10 +22,18 @@ class ChatProvider extends ChangeNotifier {
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messageList.add(newMessage);
 
+    if (text.endsWith('?')){
+      herReply();
+    }
+
     // Esto se usa para los providers, para notificar a los demas que de este provider algo cambio
     // que seria notificado al chatScreen en la linea 34
     notifyListeners();
     moveScrollToBottom();
+  }
+
+  Future<void> herReply() async{
+    final herMessage = await getYesNoAnswer.getAnswer();
   }
 
   Future<void> moveScrollToBottom() async {
